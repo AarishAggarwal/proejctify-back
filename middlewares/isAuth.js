73dee2bb3cfa1f4338b218ee16544fec811e6e43
AarishAggarwal <aaggarwal4@jpischool.com>
2025-07-05 14:38:13 +0530

    Fix Vercel

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -6,18 +6,18 @@ const isAuth=async (req,res,next)=>{
         let {token}=req.cookies
 
         if(!token){
-            return res.status(400).json({message:"user doesn't have token"})
+            return res.status(401).json({message:"Authentication required"})
         }
         let verifyToken= jwt.verify(token,process.env.JWT_SECRET)
         if(!verifyToken){
-            return res.status(400).json({message:"user doesn't have valid token"})
+            return res.status(401).json({message:"Invalid or expired token"})
         }
         
         req.userId=verifyToken.userId
         next()
     } catch (error) {
         console.log(error)
-        return res.status(500).json({message:"is auth error"})
+        return res.status(401).json({message:"Authentication failed"})
     }
 }
 
